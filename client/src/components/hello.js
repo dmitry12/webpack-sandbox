fe2edb81058513a4bc5370d3bc82e45adb9dc49a
Dmitry Astapenko <dmitry@colourbox.com>
2015-12-17 17:57:55 +0100

    Use react-css-modules as decorator

--- client/src/components/hello.js
+++ client/src/components/hello.js
@@ -3,10 +3,9 @@ import React from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './hello.css';
 
-class Hello extends React.Component {
+@CSSModules(styles)
+export default class extends React.Component {
     render() {
         return <h1 styleName="hello">Hello</h1>
     }
 }
-
-export default CSSModules(Hello, styles);
