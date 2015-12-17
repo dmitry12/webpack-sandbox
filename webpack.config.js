bb4bc9efaa7a16c280a33cbf5683a9c0e0afe76a
Dmitry Astapenko <dmitry@colourbox.com>
2015-12-17 17:53:20 +0100

    Add support for react-css-modules with devserver
    Improves babel es6 capabilities including decorators

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,65 @@
+var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 
-var config = {
+var devServer = {
+    contentBase: path.resolve(__dirname, './client/dist'),
+    colors: true,
+    quiet: false,
+    noInfo: false,
+    publicPath: '/static/',
+    historyApiFallback: false,
+    host: '127.0.0.1',
+    port: 8000
+};
+
+module.exports = {
+    devtool: 'source-map',
+    debug: true,
+    devServer: devServer,
+    context: path.resolve(__dirname, './client/src'),
     entry: {
-        'main': './client/src/js/main.js'
+        app: [
+            './'
+        ]
     },
     output: {
-        filename: './client/dist/[name].js'
+        path: path.resolve(__dirname, './dist/static'),
+        filename: '[name].js',
+        publicPath: devServer.publicPath
     },
-    watch: true,
     module: {
         loaders: [
             {
-                test: /.jsx?$/,
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!cssnext-loader')
+            },
+            {
+                test: /.js?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['es2015', 'react']
+                    plugins: ['transform-decorators-legacy', 'transform-runtime'],
+                    presets: ['es2015', 'react', 'stage-0']
                 }
-            },
-            {
-                test:   /\.css$/,
-                loader: "style-loader!css-loader!postcss-loader!cssnext-loader"
             }
         ]
     },
-    postcss: function() {
-        return [autoprefixer, cssnext]
+    plugins: [
+        new ExtractTextPlugin('app.css', {
+            allChunks: true
+        })
+    ],
+    postcss: [
+        autoprefixer, cssnext
+    ],
+    watch: true,
+    resolve: {
+        extensions: [
+            '',
+            '.js'
+        ]
     }
-}
-
-module.exports = config;
-
+};
